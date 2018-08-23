b37d65c9fc7ede81800c0e5e3253cb439ef59a2f
Nic McPhee <mcphee@morris.umn.edu>
2018-08-23 12:00:40 -0500

    Create `lein` app
    These changes come from creating a `lein` app with the relevant infrastructure.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to git-stats-csv
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/git_stats_csv/core_test.clj
@@ -0,0 +1,7 @@
+(ns git-stats-csv.core-test
+  (:require [clojure.test :refer :all]
+            [git-stats-csv.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
